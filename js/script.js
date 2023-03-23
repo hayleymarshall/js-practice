55daf207d811043010ef84eb1f81b404e071254f
Julia <uvojnarovskaa@gmail.com>
2023-03-23 16:34:25 +0200

    Условия

--- js/script.js
+++ js/script.js
@@ -16,3 +16,36 @@ const arr = [1, 2, 3]; //массив - перечень информации п
 //у каждой сущности есть определенный номер по порядку (начинаем с 0). порядок играет важную роль, мы используя порядковый номер можем обращаться к этим сущностям
 const obj = {a: 1, b: 2}; //объект 
 console.log(arr[2]);
+
+
+// Условия 
+if (4 == 4) {
+	console.log('Ok!');
+} else {
+	console.log('error');
+}
+
+
+// if (num < 49) {
+// 	console.log('error');
+// } else if (num > 100) {
+// 	console.log ('много');
+// } else {
+// 	console.log('ок');
+// }
+
+const num = 50;
+switch (num) {
+	case 49: 
+		console.log('no');
+		break;
+	case 100: 
+		console.log('no');
+		break;
+	case 70: 
+		console.log('Yes');
+		break;
+	default:
+		console.log('does not work');
+		break;
+}
